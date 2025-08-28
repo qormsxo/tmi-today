58b8356ee17181c977764dfc153980f34f9ee99b
qormsxo <geuntae0212@gmail.com>
2025-08-28 19:19:30 +0900

    ✨ 스웨거 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { WinstonLogger } from './config/logging/logger';
 import { AllExceptionsFilter } from './middlewares/filters/all-exception.filter';
 import { LoggingInterceptor } from './middlewares/interceptors/logging.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 
@@ -15,6 +16,15 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost), logger));
   // 글로벌 인터셉터 등록
   app.useGlobalInterceptors(new LoggingInterceptor(logger));
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('TMI Today API')
+    .setDescription('TMI Today API 문서')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
